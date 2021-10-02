c762e4c6568da8172b13f3a1fef88c30e3afef52
vipul <petkar.vipul@gmail.com>
2021-10-02 11:32:31 +0530

    eridium link udated

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,11 +159,11 @@ gsap.set(".shape", {
 
 document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
 t.style.left = n.clientX + "px", 
-t.style.top = n.clientY + "px", 
+t.style.top = n.clientY + 1000 + "px", 
 e.style.left = n.clientX + "px", 
-e.style.top = n.clientY + "px", 
+e.style.top = n.clientY + 1000 + "px", 
 i.style.left = n.clientX + "px", 
-i.style.top = n.clientY + "px"
+i.style.top = n.clientY + 1000 + "px"
 });
 var t = document.getElementById("cursor"),
     e = document.getElementById("cursor2"),
